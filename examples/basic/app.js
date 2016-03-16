de86db8e6dfd0569c799591a8747c1af15df3227
Thibaud Colas <thibaudcolas@gmail.com>
2016-03-16 23:34:37 +0200

    First working commit

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Select from '../../lib/index';
 
+const options = [
+    { value: 'cs', label: 'Czech' },
+    { value: 'en', label: 'English' },
+    { value: 'fi', label: 'Finnish' },
+    { value: 'fr', label: 'French' },
+];
+
 const App = React.createClass({
   render() {
     return (
       <div className="example">
         <h1>React Simpler Select example</h1>
-        <Select/>
+        <Select placeholder="Pick an item" options={options} />
       </div>
     );
   }
